fa3883773a4a513d67f56e2f056105c504bc77dd
sohail-coder <shaiksohail2332000@gmail.com>
2022-04-10 16:00:29 +0530

    postConstruct and preDestroy is done

--- src/main/java/spring/MyApp.java
+++ src/main/java/spring/MyApp.java
@@ -4,10 +4,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
         Coach coach = context.getBean("tennisCoach",Coach.class);
         System.out.println(coach.getDialyWorkout());
         System.out.println(coach.getDailtFortune());
+//        Runtime.getRuntime().addShutdownHook(new Thread() {
+//            public void run() {
+//                context.close();
+//            }});
+        context.close();
 
     }
 
--- src/main/java/spring/TennisCoach.java
+++ src/main/java/spring/TennisCoach.java
@@ -3,12 +3,17 @@ package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class TennisCoach implements Coach{
     @Autowired
-    @Qualifier("databaseFortunr")
+    @Qualifier("happyFortune")
     private FortuneService fortuneService;
 //    @Autowired
 //    TennisCoach(FortuneService fortuneService){
@@ -28,4 +33,24 @@ public class TennisCoach implements Coach{
     public String getDailtFortune() {
         return fortuneService.getFortuneService();
     }
+
+    //init method
+   @PostConstruct
+    public void initMethod(){
+        System.out.println("in init method");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("in destroy method");
+    }
+
+
+
+
+
+
+
+
+
 }
